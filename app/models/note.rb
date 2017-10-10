db57aef1d278687de232f3fd97fca7c2fefa1f56
Dmitry Ilin <dailyin@luch.podolsk.ru>
2017-10-10 15:57:39 +0300

    Update some for notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ActiveRecord::Base
   attr_accessible :content, :status, :review
 
-  STATUS = %w(new failed done)
+  STATUS = %w(new failed done later)
 
   validates :content, presence: true
 end

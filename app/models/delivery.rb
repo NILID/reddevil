bc1b49593378c1f1559fd196945466d42bc37104
Dmitry Ilin <dailyin@luch.podolsk.ru>
2017-12-12 16:36:23 +0300

    Current show empty deliver. add validates presence

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,4 +1,8 @@
 class Delivery < ActiveRecord::Base
   belongs_to :purchase
   attr_accessible :delivery, :doc
+
+  validates :doc, presence: true, unless: :delivery?
+  validates :delivery, presence: true, unless: :doc?
+
 end

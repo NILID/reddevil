6b2fc4f64a653d29617d59a8db8a1cd19cfdeafc
Dmitry Ilin <dailyin@luch.podolsk.ru>
2017-12-12 16:13:35 +0300

    Rebuild date helper

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -1,10 +1,12 @@
 module PurchasesHelper
   def short_date(item)
-    item = item.to_date
-    if DateTime.now.year == item.year
-      Russian.strftime(item, '%d %b') if item
-    else
-      short_date_year(item)
+    if item
+      item = item.to_date
+      if DateTime.now.year == item.year
+        Russian.strftime(item, '%d %b')
+      else
+        short_date_year(item)
+      end
     end
   end
 

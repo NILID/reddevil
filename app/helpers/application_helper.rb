b00521f8658f2b01ef69477bc2dcc5dc489fce92
Dmitry Ilin <dailyin@luch.podolsk.ru>
2018-04-27 08:32:22 +0300

    Update some styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
 
    def plural(count, value)
-     val="plural.#{value}"
-     result=Russian::p(count, t("#{val}_1"), t("#{val}_2"), t("#{val}_10"))
+     val = "plural.#{value}"
+     result = Russian::p(count, t("#{val}_1"), t("#{val}_2"), t("#{val}_10"))
      count.to_s + ' ' + result
    end
 
    def arttime(time)
-     Russian.strftime(time, "%A  %d.%m.%Y")
+     Russian.strftime(time, "%A %d.%m.%Y")
    end
 
    def deadline(time)
-     Russian.strftime(time, "%A  %d.%m.%Y %H:%M")
+     Russian.strftime(time, "%d %B %Y %H:%M (%A)")
    end
 end

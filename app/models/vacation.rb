08e39c8b1bfa0b31fc0aaa5cb222ea717389e103
Dmitry Ilin <dailyin@luch.podolsk.ru>
2018-06-08 13:36:35 +0300

    Fix errros

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -3,7 +3,7 @@ class Vacation < ActiveRecord::Base
 
   validates :startdate, :enddate, presence: true
 
-  scope :old, lambda { where('enddate < ?', DateTime.now) }
-  scope :future, lambda { where('startdate >= ?', DateTime.now) }
-
+  scope :old,     lambda { where('enddate < ?', DateTime.now) }
+  scope :future,  lambda { where('startdate >= ?', DateTime.now) }
+  scope :present, lambda { where('startdate <= ?', DateTime.now).where('enddate >= ?', DateTime.now) }
 end

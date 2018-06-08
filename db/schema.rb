22d6b0d394303ee60d5e4e12dff2222eda7e2272
Dmitry Ilin <dailyin@luch.podolsk.ru>
2018-06-08 08:01:32 +0300

    update ruby and gems

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180607060011) do
+ActiveRecord::Schema.define(version: 20180608050004) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title",      limit: 255

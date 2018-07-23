43d723c4391d5202eb8326ffdffd8d858068e894
da <da@reddevil.luch.podolsk.ru>
2018-07-23 15:05:55 +0300

    Update gems

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,9 +1,12 @@
 class Vacation < ActiveRecord::Base
   belongs_to :member
 
-  validates :startdate, :enddate, presence: true
+  validates :startdate,
+            :enddate,
+            presence: true
 
-  scope :old,     lambda { where('enddate < ?', DateTime.now) }
+  scope :old,     lambda { where('enddate < ?',    DateTime.now) }
   scope :future,  lambda { where('startdate >= ?', DateTime.now) }
-  scope :present, lambda { where('startdate <= ?', DateTime.now).where('enddate >= ?', DateTime.now) }
+  scope :present, lambda { where('startdate <= ?', DateTime.now)
+                          .where('enddate >= ?',   DateTime.now) }
 end

23b63646ef12f878f08c06e47b32b0ed8c2fbe04
da <da@reddevil.luch.podolsk.ru>
2018-08-13 09:20:03 +0300

    Update gems

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,25 +8,25 @@ class Ability
     # everybody
     can :read, :all
     can %i[archive stat holidays get_holidays], Member
-    cannot [:manage, :read], [Message, Folder, Dataset, Substrate, Year, Machine, Task]
-    can [:new, :create], Note
-    cannot [:edit, :update], Profile
-    # cannot :read, Doc, category: {hidden: true}
+    cannot %i[manage read], [Message, Folder, Dataset, Substrate, Year, Machine, Task]
+    can %i[new create], Note
+    cannot %i[edit update], Profile
+    # cannot :read, Doc, category: { hidden: true }
     can :rebuild, Result
     cannot :read, [Art, Work, Forecast, Song, Album, Round, Forecast, Type, User]
     cannot :mirrors, Substrate
     can :list, [Event]
 
     if user.role? :user
-      can [:edit, :update], Profile, user_id: user.id
+      can %i[edit update], Profile, user_id: user.id
       can :read, :all
       can %i[destroy edit update], Forecast do |f|
         (f.round.deadline > DateTime.now) && (f.user_id == user.id)
       end
-      # cannot :read, Doc, category: {hidden: true}
+      # cannot :read, Doc, category: { hidden: true }
 
       cannot :read, [Song, Album, Art, Work, Forecast, Round, Substrate, Year, Message, Type, User]
-      can [:favorites], Subscribe
+      can :favorites, Subscribe
       cannot :download, Round, check_finish?: false
     end
 
@@ -36,7 +36,7 @@ class Ability
     end
 
     if user.role? :editor
-      can [:manage, :read], Message
+      can %i[manage read], Message
     end
 
     if user.role? :admin
@@ -44,26 +44,26 @@ class Ability
         (f.round.deadline < DateTime.now) || (f.user_id == user.id)
       end
 
-      can [:read, :manage], :all
+      can %i[read manage], :all
 
       cannot :manage, Work
-      can [:import], Subscribe
-      cannot [:read, :manage], [Dataset, Folder]
+      can :import, Subscribe
+      cannot %i[read manage], [Dataset, Folder]
 
       can :counted, Result
-#      can :read, Material, groups_mask: user.groups_mask
-      cannot [:edit, :update], Art do |art|
+      # can :read, Material, groups_mask: user.groups_mask
+      cannot %i[edit update], Art do |art|
         art.closed?
       end
-      can [:make_role, :edit_roles], User
+      can %i[make_role edit_roles], User
       cannot :read, [Art, Work, Song, Album, Round, Forecast]
       cannot :download, Round, check_finish?: false
       can :remote_show, Substrate
     end
 
     if (user.role? :admin) || (user.role? :moderator) || (user.role? :editor) || (user.role? :user)
-      can [:manage, :read], Folder,  user:   { id: user.id }
-      can [:manage, :read], Dataset, folder: { user_id: user.id }
+      can %i[manage read], Folder,  user:   { id: user.id }
+      can %i[manage read], Dataset, folder: { user_id: user.id }
     end
 
     if user.role? :drawing
@@ -78,29 +78,29 @@ class Ability
 
     if user.has_group? :art
       can :read, [Art, Work]
-      can [:new, :create], Art
-      can [:new, :create], Work, art: { closed?: false }
+      can %i[new create], Art
+      can %i[new create], Work, art: { closed?: false }
       can %i[edit update destroy], Work, user: { id: user.id }, art: { closed?: false }
       can :manage, Source, work: { user_id: user.id }
     end
 
     if user.has_group? :lab193
       can %i[manage read download], [Song, Album]
-      can [:get_results], Match
-      can [:read], [Art, Work, Round]
+      can :get_results, Match
+      can :read, [Art, Work, Round]
       can :like, Song
-      can [:favorites, :list], Album
+      can %i[favorites list], Album
 
-      can [:new, :create], Forecast, user: {id: user.id}
+      can %i[new create], Forecast, user: { id: user.id }
     end
 
     if user.has_group? :sellers
-      can [:manage, :read], [Purchase, Year]
+      can %i[manage read], [Purchase, Year]
     end
 
     if user.has_group? :machine
       can :read, Machine
-      can [:manage, :read], Task
+      can %i[manage read], Task
     end
   end
 end

682215b242ea946d5fe54e87fec25529b7c08602
da <da@reddevil.luch.podolsk.ru>
2018-10-04 13:49:30 +0300

    update css for song

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,19 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :confirmable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email,
+                  :password,
+                  :password_confirmation,
+                  :remember_me
   delegate :login, to: :profile
 
   attr_accessible :groups, :roles, :sport_flag, as: :admin

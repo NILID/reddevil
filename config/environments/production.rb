1568d3794bae9e2506d231313c8a7d63c188fef9
Dmitry Ilin <dailyin@luch.podolsk.ru>
2019-05-07 10:19:15 +0300

    Add config for custom exceptions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.exceptions_app = self.routes
+
   config.action_mailer.default_url_options = {:host => 'reddevil.luch.podolsk.ru', from: '"RedDevil" <reddevil@luch.podolsk.ru>'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true

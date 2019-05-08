38df67c771b0464542aec6386d995a821370bdd3
Dm <you@example.com>
2019-05-08 14:54:25 +0300

    Update avatars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @users = @users.includes(:profile)
+    @users = @users.includes(:profile, :member)
   end
 
   def make_role

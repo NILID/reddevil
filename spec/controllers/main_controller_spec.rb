b8e8acec9952eba40f2ed84512d67e5972d02ce8
Dm <you@example.com>
2019-05-21 14:59:46 +0300

    Move calendar to user_sessions

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe MainController, type: :controller do
+
+  describe 'user should' do
+    login_user(:user)
+
+    it 'get calendar' do
+      get :calendar
+      expect(response).to be_success
+      expect(response).to render_template(:calendar)
+    end
+  end
+
   describe 'unreg user should' do
     it 'get index' do
       get :index
       expect(response).to be_success
       expect(response).to render_template(:index)
+    end
+
+    it 'not get calendar' do
+      get :calendar
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end

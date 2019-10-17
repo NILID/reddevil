91088a9f5d59e2f5f63548c107f2e3302e51fc9f
Dm <you@example.com>
2019-10-17 11:16:48 +0300

    Update online status

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,11 @@
+module UsersHelper
+  def last_seen(user)
+    if user.online?
+      content_tag(:span, t('users.online'), class: 'badge badge-success')
+    else
+      time_ago = distance_of_time_in_words(DateTime.now, user.updated_at)
+      time_ago_text = t('users.online_ago_html', time: time_ago)
+      content_tag(:small, time_ago_text, class: 'text-muted' )
+    end
+  end
+end

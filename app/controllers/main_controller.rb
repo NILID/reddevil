abeae4f35f6d2d7c16b2427e86cae2c746b71e3e
Dmitry <dailyin@luch.podolsk.ru>
2020-02-26 14:37:36 +0300

    Refactoring

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -16,17 +16,13 @@ class MainController < ApplicationController
       @bdusers_tomorrow = @bduser.find_births_for(tomorrow).group_by {|u| [u.birth.strftime("%m"), u.birth.strftime("%d")]}
       @bdusers_month    = @bduser.find_births_for(tomorrow + 1.day, now + 30.days).group_by {|u| [(u.birth.month < DateTime.now.month ? 1 : 0), u.birth.strftime("%m"), u.birth.strftime("%d")]}
 
-      @vacations      = Vacation.where('startdate <= ?', now)
-                                .where('enddate >= ?',   now)
-                                .where(flag: 'rest')
-                                .order(:enddate)
-                                .includes(:member)
+      all_vacations     = Vacation.where('startdate <= ?', now)
+                                  .where('enddate >= ?',   now)
+                                  .order(:enddate)
+                                  .includes(:member)
 
-      @sickdays      = Vacation.where('startdate <= ?', now)
-                                .where('enddate >= ?',   now)
-                                .where(flag: 'sick')
-                                .order(:enddate)
-                                .includes(:member)
+      @vacations      = all_vacations.where(flag: 'rest')
+      @sickdays       = all_vacations.where(flag: 'sick')
 
       @vacations_soon = Vacation.where('startdate > ?',  DateTime.now)
                                 .where('startdate <= ?', DateTime.now + 7.days)

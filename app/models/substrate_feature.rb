bf12ca0b13153c2a7c26a7efd9fafc5b54311cbe
Dmitry <dailyin@luch.podolsk.ru>
2020-10-29 12:39:20 +0300

    Rebuild regular for substrate

--- app/models/substrate_feature.rb
+++ app/models/substrate_feature.rb
@@ -6,7 +6,7 @@ class SubstrateFeature < ApplicationRecord
   validates :length, :sign, :litera, :feature, presence: true
   validates :feature,
             numericality: { greater_than_or_equal_to: 0 }
-  validates :length, format: { with: /\A[0-9]+\-{0,1}(\.\.\.){0,1}[0-9]+\z/ }
+  validates :length, format: { with: /(\A\d+\-{0,1}(\.\.\.){0,1}\d+\z|\A\d+\z)/ }
   validates_inclusion_of :sign,   in: SIGNS
   validates_inclusion_of :litera, in: %w[R T]
 end

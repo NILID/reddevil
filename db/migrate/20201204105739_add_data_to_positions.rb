85adbedf428bf99b2d07285ed3579efe070f21c3
Dmitry <dailyin@luch.podolsk.ru>
2020-12-09 17:08:51 +0300

    Add positions data

--- db/migrate/20201204105739_add_data_to_positions.rb
+++ db/migrate/20201204105739_add_data_to_positions.rb
@@ -1,7 +1,7 @@
 class AddDataToPositions < ActiveRecord::Migration[5.2]
   def change
     Member.all.each do |member|
-      Position.create! member: member, department: member.department, position: member.position
+      Position.create! member: member, department: member.department, position: member.position, move_at: DateTime.now
     end
   end
 end

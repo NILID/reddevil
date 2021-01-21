ef3963c3dc04441b6ca6d30315d804af5a5ee923
Dmitry <dailyin@luch.podolsk.ru>
2021-01-21 19:29:56 +0300

    Add validation

--- app/models/manufacture_group.rb
+++ app/models/manufacture_group.rb
@@ -1,4 +1,8 @@
 class ManufactureGroup < ApplicationRecord
   has_many :manufactures
   validates :title, presence: true
+  validates :contract,
+            presence: true,
+            if: -> { without_contract == false }
+
 end
--- spec/models/manufacture_group_spec.rb
+++ spec/models/manufacture_group_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe ManufactureGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:manufacture_group) { build_stubbed(:manufacture_group) }
+
+  context 'should' do
+    it 'have title' do
+      manufacture_group.title = nil
+      expect(manufacture_group.valid?).to be false
+      expect(manufacture_group.errors[:title]).not_to be_empty
+    end
+
+    it 'have contract' do
+      manufacture_group.contract = nil
+      expect(manufacture_group.valid?).to be false
+      expect(manufacture_group.errors[:contract]).not_to be_empty
+    end
+
+    it 'have not contract if without_contract is true' do
+      manufacture_group.contract = nil
+      manufacture_group.without_contract = true
+      expect(manufacture_group.valid?).to be true
+      expect(manufacture_group.errors[:contract]).to be_empty
+    end
+
+  end
 end

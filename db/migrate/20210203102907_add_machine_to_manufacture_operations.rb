8704f8a7a8447a51696005bd2060f30481267ef8
Dmitry Ilin <dailyin@luch.podolsk.ru>
2021-02-04 13:48:36 +0300

    Update migration

--- db/migrate/20210203102907_add_machine_to_manufacture_operations.rb
+++ db/migrate/20210203102907_add_machine_to_manufacture_operations.rb
@@ -1,8 +1,9 @@
 class AddMachineToManufactureOperations < ActiveRecord::Migration[5.2]
   def change
     add_column :manufacture_operations, :machine, :string, default: ''
+#    remove_column :manufacture_operations, :machine, :string, default: ''
     ManufactureOperation.all.each do |mo|
-      mo.update_attribute(machine: mo.manufacture.machine) if mo.manufacture && mo.manufacture.machine?
+      mo.update_attribute(:machine, mo.manufacture.machine) if mo.manufacture && mo.manufacture.machine?
     end
   end
 end
